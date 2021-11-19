f78f9bdacb0a3070408960a13862e778603123a4
Teddy Sahr <tedward1218@gmail.com>
2021-11-18 20:45:53 -0500

    fixed tags route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   const tags = await Tag.findAll({
-    include: [{ model: Product }, { model: ProductTag }],
+    include: [{ model: Product }],
   }).catch((err) => {
     res.json(err);
   });
@@ -19,13 +19,13 @@ router.get("/:id", async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tag = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: ProductTag }],
+      include: [{ model: Product }],
     });
 
     if (tag !== null) {
       res.status(200).json(tag);
     } else {
-      res.status(404).json({ message: "category not found" });
+      res.status(404).json({ message: "tag not found" });
     }
   } catch (err) {
     res.status(500).json(err);
